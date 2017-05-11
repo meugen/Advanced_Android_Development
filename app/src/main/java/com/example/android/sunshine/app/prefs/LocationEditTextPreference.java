f09e117b52e7002410e5ea49621a8424983a316d
meugen <meugen.82@gmail.com>
2017-05-11 23:02:55 +0300

    Implemented check min length

--- app/src/main/java/com/example/android/sunshine/app/prefs/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/prefs/LocationEditTextPreference.java
@@ -1,10 +1,18 @@
 package com.example.android.sunshine.app.prefs;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.android.sunshine.app.R;
 
@@ -12,7 +20,7 @@ import com.example.android.sunshine.app.R;
  * @author meugen
  */
 
-public class LocationEditTextPreference extends EditTextPreference {
+public class LocationEditTextPreference extends EditTextPreference implements TextWatcher {
 
     private static final String TAG = LocationEditTextPreference.class.getSimpleName();
 
@@ -40,5 +48,29 @@ public class LocationEditTextPreference extends EditTextPreference {
 
     public LocationEditTextPreference(final Context context) {
         super(context);
+    }
+
+    @Override
+    protected void showDialog(final Bundle state) {
+        super.showDialog(state);
+        getEditText().addTextChangedListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(
+            final CharSequence s, final int start, final int count, final int after) {}
+
+    @Override
+    public void onTextChanged(
+            final CharSequence s, final int start, final int before, final int count) {}
+
+    @Override
+    public void afterTextChanged(final Editable s) {
+        final Dialog dialog = getDialog();
+        if (dialog instanceof AlertDialog) {
+            final AlertDialog alertDialog = (AlertDialog) dialog;
+            final Button button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            button.setVisibility(s.length() >= minLength ? View.VISIBLE : View.INVISIBLE);
+        }
     }
 }
